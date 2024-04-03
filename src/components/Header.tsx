9903c1b5a94347b9e53125df0f03204ae04bc3d6
Emkay2309 <emkay2309@gmail.com>
2024-04-04 00:01:06 +0530

    fixed type

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className="bg-[#ffffff] flex  justify-around items-center flex-row my-4">
                 <h1 className="text-[#CF2D2D] text-xl  font-bold">BookUsNow</h1>
                 <div className="flex gap-2 w-1/3">
-                    <button className="sm:block hidden w-1/5 bg-black  rounded-lg text-white text-semibold ">Catogories</button>
+                    <button className="sm:block hidden w-1/5 bg-black  rounded-lg text-white text-semibold ">Cateogories</button>
                     <div className=" w-4/5 flex items-center relative ">
                         <input className="sm:block hidden px-2 py-1 w-full border-[#B0BABF] border-2  rounded-lg " placeholder="Type..."></input>
                         <span className="absolute flex  right-2 "><FaSearch /></span>
